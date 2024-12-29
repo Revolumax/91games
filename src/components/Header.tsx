eb1bf62d2cba6803afcc013289a31364ba61b742
Revolumax <oscaralexander520@gmail.com>
2024-12-29 12:22:38 -0400

    91games

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -158,6 +158,7 @@ export default function Header() {
           <li>
             <Link
               href="/Quienessomos"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="block py-2 pl-3 pr-4 hover:bg-purple-700 text-white border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Quienes Somos
@@ -166,6 +167,7 @@ export default function Header() {
           <li>
             <Link
               href="/Eventos"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="block py-2 pl-3 pr-4 text-white hover:bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:hover:bg-gray-700 dark:text-gray-400"
             >
               Eventos
@@ -174,6 +176,7 @@ export default function Header() {
           <li>
             <Link
               href="/Ganadores"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="block py-2 pl-3 pr-4 text-white hover:bg-purple-700 border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Ganadores
@@ -182,6 +185,7 @@ export default function Header() {
           <li>
             <Link
               href="/Torneos"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="block py-2 pl-3 pr-4 dark:text-gray-400 hover:bg-purple-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Torneos
